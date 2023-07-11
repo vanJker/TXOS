1e23339c4882ea3187798537ac28019381bcd5b1
vanJker <1847722163@qq.com>
2023-07-11 17:12:14 +0800

    finish #[023 varargs]

---
+++ src/include/xos/stdarg.h
@@ -0,0 +1,10 @@
+#ifndef XOS_STDARG_H
+#define XOS_STDARG_H
+
+typedef char *va_list;
+
+#define va_start(ap, v) (ap = (va_list)&v + sizeof(char *))
+#define va_arg(ap, t) (*(t *)((ap += sizeof(char *)) - sizeof(char *)))
+#define va_end(ap) (ap = (va_list)0)
+
+#endif
--- src/kernel/main.c
+++ src/kernel/main.c
@@ -3,16 +3,22 @@
 #include <xos/io.h>
 #include <xos/string.h>
 #include <xos/console.h>
+#include <xos/stdarg.h>
 
-char msg[] = "[INFO] Hello, XOS!!!\n";
-u8 buf[1024];
+void test_varargs(int cnt, ...) {
+    va_list args;
+    va_start(args, cnt);
+
+    int arg;
+    while (cnt--) {
+        arg = va_arg(args, int);
+    }
+
+    va_end(args);
+}
 
 void kernel_init() {
     console_init();
-
-    while (true) {
-        console_write(msg, strlen(msg), INFO);
-    }
-
+    test_varargs(5, 1, 0x55, 0xaa, 4, 0xffff);
     return;
 }
