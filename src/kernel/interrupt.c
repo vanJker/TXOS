b49b8ba630f3831fa6ab49e367efc8b27635e124
vanJker <1847722163@qq.com>
2023-08-01 16:48:22 +0800

    补充一些说明

--- src/kernel/interrupt.c
+++ src/kernel/interrupt.c
@@ -81,17 +81,17 @@ void default_handler(int vector) {
 // 初始化中断控制器
 void pic_init() {
     outb(PIC_M_CTRL, 0b00010001); // ICW1: 边沿触发, 级联 8259, 需要ICW4.
-    outb(PIC_M_DATA, 0x20);       // ICW2: 起始端口号 0x20
+    outb(PIC_M_DATA, 0x20);       // ICW2: 起始端口/中断向量号 0x20
     outb(PIC_M_DATA, 0b00000100); // ICW3: IR2接从片.
     outb(PIC_M_DATA, 0b00000001); // ICW4: 8086模式, 正常EOI
 
     outb(PIC_S_CTRL, 0b00010001); // ICW1: 边沿触发, 级联 8259, 需要ICW4.
-    outb(PIC_S_DATA, 0x28);       // ICW2: 起始端口号 0x28
+    outb(PIC_S_DATA, 0x28);       // ICW2: 起始端口/中断向量号 0x28
     outb(PIC_S_DATA, 2);          // ICW3: 设置从片连接到主片的 IR2 引脚
     outb(PIC_S_DATA, 0b00000001); // ICW4: 8086模式, 正常EOI
 
-    outb(PIC_M_DATA, 0b11111110); // 关闭所有中断
-    outb(PIC_S_DATA, 0b11111111); // 关闭所有中断
+    outb(PIC_M_DATA, 0b11111110); // OCW1: 关闭所有中断（时钟中断除外）
+    outb(PIC_S_DATA, 0b11111111); // OCW1: 关闭所有中断
 }
 
 // 初始化中断描述符表，以及中断处理函数表
